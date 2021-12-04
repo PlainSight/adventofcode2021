0776c8e3c8cb5312bbc43a14511642b9c9c10084
Alex <alexander.ogier@gmail.com>
2021-12-04 20:13:20 +1300

    day4

--- day4/part2.js
+++ day4/part2.js
@@ -1,3 +1,58 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n\r\n');
+
+var order = input[0].split(',').map(n => parseInt(n));
+
+var boards = [];
+
+for (var b = 1; b < input.length; b++) {
+    boards.push(input[b].split('\r\n').map(l => l.trim().split(/\s+/).map(n => parseInt(n))));
+}
+
+for(var r = 0; r < order.length; r++) {
+    boards = boards.filter(b => !b.complete);
+    boards.forEach(b => {
+        b.forEach((l, li) => l.forEach((c, ci) => {
+                if (c == order[r]) {
+                    b[li][ci] = -1;
+                }
+            })
+        );
+
+        var complete = false;
+        for(var row = 0; row < 5; row++) {
+            var completedColumn = true;
+            for(var col = 0; col < 5; col++) {
+                if (b[row][col] != -1) {
+                    completedColumn = false;
+                }
+            }
+            if (completedColumn) {
+                complete = true;
+            }
+        }
+
+        for(var col = 0; col < 5; col++) {
+            var completedRow = true;
+            for(var row = 0; row < 5; row++) {
+                if (b[row][col] != -1) {
+                    completedRow = false;
+                }
+            }
+            if (completedRow) {
+                complete = true;
+            }
+        }
+        if (complete) {
+            var sum = 0;
+            b.forEach((l, li) => l.forEach((c, ci) => {
+                if (b[li][ci] != -1) {
+                    sum += b[li][ci];
+                }
+            }));
+            b.complete = true;
+            console.log(r, sum * order[r], b);
+        }
+    });
+}
