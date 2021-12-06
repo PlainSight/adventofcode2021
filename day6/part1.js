e12658c57a4232004392a558a8c022abc989a2f7
Alex <alexander.ogier@gmail.com>
2021-12-06 18:41:13 +1300

    day 6

--- day6/part1.js
+++ day6/part1.js
@@ -1,3 +1,22 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\n');
+var fish = fs.readFileSync('./input.txt', 'utf8').split(',').map(i => parseInt(i));
+
+var days = 0;
+
+while(days < 80) {
+    // turn 0s into new fish
+    var reproducing = fish.filter(f => f == 0);
+
+    var newFish = fish.map(f => f == 0 ? 6 : f-1);
+
+    reproducing.forEach(r => {
+        newFish.push(8);
+    })
+
+    fish = newFish;
+
+    days++;
+}
+
+console.log(fish.length);
