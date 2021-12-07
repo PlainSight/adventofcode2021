216927785c1c581da3b40fc00bc9a202a8486ed8
Alexander <alexander.ogier@trademe.co.nz>
2021-12-07 21:53:14 +1300

    day7

--- day7/part2.js
+++ day7/part2.js
@@ -1,3 +1,27 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split(',').map(c => parseInt(c));
+
+var min = Math.min(...input);
+var max = Math.max(...input);
+
+var lowestSum = Infinity;
+var lowestPosition = -1;
+
+for(var i = min; i < max; i++) {
+    var sum = 0;
+    input.forEach(v => {
+        sum += dist(i, v);
+    });
+    if (sum < lowestSum) {
+        lowestSum = sum;
+        lowestPosition = i;
+    }
+}
+
+console.log(lowestPosition, lowestSum);
+
+function dist(i, v) {
+    var d = v == i ? 0 : (1 + Math.abs(v - i));
+    return (d*0.5*Math.abs(v-i));
+}
