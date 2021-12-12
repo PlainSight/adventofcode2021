d5a6af4676b323ce1db263fdb6257cb4465b5676
Alex <alexander.ogier@gmail.com>
2021-12-12 19:52:03 +1300

    day12

--- day12/part1.js
+++ day12/part1.js
@@ -1,3 +1,40 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+
+var connections = {};
+
+input.forEach(i => {
+    var parts = i.split('-');
+
+    connections[parts[0]] = (connections[parts[0]] || []);
+    connections[parts[0]].push(parts[1]);
+
+    connections[parts[1]] = (connections[parts[1]] || []);
+    connections[parts[1]].push(parts[0]);
+})
+
+console.log(connections);
+
+var paths = 0;
+
+function explore(next, visited) {
+    if (next == 'end') {
+        paths++;
+        return;
+    }
+    var newVisited = JSON.parse(JSON.stringify(visited));
+    if (/[a-z]+/.test(next)) {
+        newVisited.push(next);
+    }
+
+    connections[next].forEach(c => {
+        if (!newVisited.includes(c)) {
+            explore(c, newVisited);
+        }
+    })
+}
+
+explore('start', []);
+
+console.log(paths);
--- day12/part2.js
+++ day12/part2.js
@@ -1,3 +1,44 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+
+var connections = {};
+
+input.forEach(i => {
+    var parts = i.split('-');
+
+    connections[parts[0]] = (connections[parts[0]] || []);
+    connections[parts[0]].push(parts[1]);
+
+    connections[parts[1]] = (connections[parts[1]] || []);
+    connections[parts[1]].push(parts[0]);
+})
+
+console.log(connections);
+
+var paths = 0;
+
+function explore(next, visited) {
+    if (next == 'end') {
+        paths++;
+        return;
+    }
+    var newVisited = JSON.parse(JSON.stringify(visited));
+    if (visited.v.includes(next)) {
+        newVisited.t = true;
+    } else {
+        if (/[a-z]+/.test(next)) {
+            newVisited.v.push(next);
+        }
+    }
+
+    connections[next].forEach(c => {
+        if ((!newVisited.v.includes(c) || !newVisited.t) && c != 'start') {
+            explore(c, newVisited);
+        }
+    })
+}
+
+explore('start', { v: [], t: false});
+
+console.log(paths);
