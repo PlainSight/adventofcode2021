2d5179c2565b2c6fd764c2020151e8648a690001
Alex <alexander.ogier@gmail.com>
2021-12-14 18:57:02 +1300

    day14

--- day14/part1.js
+++ day14/part1.js
@@ -1,3 +1,32 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n\r\n');
+
+var template = input[0].split('');
+var rules = input[1].split('\r\n').map(r => { 
+    var parts = r.split(' -> ');
+    return {
+        i: parts[0].split(''),
+        o: parts[1]
+    }
+});
+
+for(var i = 0; i < 10; i++) {
+    var newTemplate = [];
+    for(var t = 0; t < template.length-1; t++) {
+        newTemplate.push(template[t]);
+        var rule = rules.filter(r => r.i[0] == template[t] && r.i[1] == template[t+1])[0];
+        newTemplate.push(rule.o);
+    }
+    newTemplate.push(template[template.length-1]);
+    template = newTemplate;
+}
+
+var freq = template.reduce((a, c) => {
+    a[c] = (a[c] || 0) + 1;
+    return a;
+}, {});
+
+var values = Object.values(freq);
+
+console.log(Math.max(...values) - Math.min(...values));
