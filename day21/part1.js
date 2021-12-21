a7579134edc2182cf483392186e1d9b64c7037b0
Alexander <alexander.ogier@trademe.co.nz>
2021-12-21 20:34:16 +1300

    day21

--- day21/part1.js
+++ day21/part1.js
@@ -1,3 +1,29 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+
+
+var positions = [5-1, 10-1];
+var scores = [0, 0];
+var rolls = 0;
+
+var turn = 0;
+
+var dedice = 1;
+function roll() {
+    rolls++;
+    var res = dedice;
+    dedice += 1;
+    return res;
+}
+
+while (scores.filter(s => s >= 1000).length == 0) {
+    var move = roll() + roll() + roll();
+    positions[turn] = (positions[turn] + move) % 10;
+    scores[turn] += (positions[turn]+1);
+    turn = (turn+1) % 2;
+}
+
+console.log(rolls);
+console.log(scores);
+console.log(rolls*Math.min(...scores));
